a70015d7f0e33ad4c39799fcc850e917adc19d1c
Nafiz <1000yearsoldguy@gmail.com>
2024-12-24 15:00:13 +0600

    Addition Calculator

--- 7th class/script.js
+++ 7th class/script.js
@@ -1,3 +1,6 @@
 function showSomething() {
-  alert("hello from showSomething function");
+  let x = Number(document.getElementById("num1").value);
+  let y = Number(document.getElementById("num2").value);
+  let z = x + y;
+  alert(z);
 }
